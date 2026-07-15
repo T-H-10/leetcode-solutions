9b72c40f4a83c3e5b7366b927b15c1598a9799d7
T-H-10 <yh9336497@gmail.com>
2026-07-15 14:31:46 +0300

    Time: 2028 ms (52.27%), Space: 26.5 MB (45.45%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.py
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.py
@@ -1,14 +1,7 @@
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-    
-class Solution(object):
-    def subsequencePairCount(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+import math
+
+class Solution:
+    def subsequencePairCount(self, nums: List[int]) -> int:
         MOD = 10**9 + 7
         
         dp = {(0, 0): 1}
@@ -19,10 +12,10 @@ class Solution(object):
             for (g1, g2), count in dp.items():
                 next_dp[(g1, g2)] = (next_dp.get((g1, g2), 0) + count) % MOD
                 
-                new_g1 = gcd(g1, x)
+                new_g1 = math.gcd(g1, x)
                 next_dp[(new_g1, g2)] = (next_dp.get((new_g1, g2), 0) + count) % MOD
                 
-                new_g2 = gcd(g2, x)
+                new_g2 = math.gcd(g2, x)
                 next_dp[(g1, new_g2)] = (next_dp.get((g1, new_g2), 0) + count) % MOD
             
             dp = next_dp
