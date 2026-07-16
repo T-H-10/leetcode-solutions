de8fef7adf11a38c9701d3096acdf5000d73c2b2
T-H-10 <yh9336497@gmail.com>
2026-07-16 12:28:23 +0300

    0485-max-consecutive-ones | Easy | .py | Array | 07-16-2026 : 11 ms (85.04%) 21.8 MB (42.73%)

---
+++ Python3/0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        cnt = 0
+        maxConsec = 0
+        for num in nums:
+            if num:
+                cnt += 1
+                if cnt > maxConsec:
+                    maxConsec = cnt
+            else:
+                cnt = 0
+        return maxConsec
